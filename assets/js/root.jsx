f8dc4098b4eaf8de71713ceff0b5e798b7d322b0
Raquel Levy <levy.r@husky.neu.edu>
2018-11-05 23:34:56 -0500

    add linked pages

--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import $ from 'jquery';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
  export default function root_init(node) {
   let tasks = window.tasks;
   ReactDOM.render(<Root tasks={tasks} />, node);
 }
+
  class Root extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tasks: props.tasks,
+      users: []
     };
-    this.fetch_tasks();
+    //this.fetch_tasks();
   }
 
   fetch_tasks() {
-    console.log("fething tasks");
     $.ajax("/api/v1/tasks", {
       method: "get",
       dataType: "json",
@@ -30,14 +32,38 @@ import $ from 'jquery';
     });
   }
 
+  fetch_users() {
+    $.ajax("/api/v1/users", {
+      method: "get",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: "",
+      success: (resp) => {
+        let state1 = _.assign({}, this.state, { users: resp.data });
+        this.setState(state1);
+      },
+    });
+  }
+
   render() {
     return <div>
-      <Header />
-      <TaskList tasks={this.state.tasks} />
+      <Router>
+        <div>
+          <Header root={this} />
+          <Route path="/" exact={true} render={() =>
+            <TaskList tasks={this.state.tasks} />
+          } />
+          <Route path="/users" exact={true} render={() =>
+            <UserList users={this.state.users} />
+          } />
+        </div>
+      </Router>
     </div>;
   }
 }
- function Header(_props) {
+
+
+ /*</div>function Header(_props) {
   return <div className="row my-2">
     <div className="col-6">
       <h1>Task Tracker</h1>
@@ -49,8 +75,26 @@ import $ from 'jquery';
         <button className="btn btn-secondary">Login</button>
       </div>
     </div>
-  </div>;
+  </div>;*/
+
+ function Header(_props) {
+  return <div className="row my-2">
+    <div className="col-4">
+      <h1><Link to={"/"}>Task Tracker</Link></h1>
+    </div>
+    <div className="col-2">
+      <p><Link to={"/users"} onClick={_props.root.fetch_users.bind(_props.root)}>Users</Link></p>
+    </div>
+    <div className="col-6">
+      <div className="form-inline my-2">
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button className="btn btn-secondary">Login</button>
+      </div>
+    </div>
+    </div>;
 }
+
  function TaskList(props) {
   let tasks = _.map(props.tasks, (t) => <Task key={t.id} task={t} />);
   return <div className="row">
@@ -66,4 +110,29 @@ import $ from 'jquery';
       assigned to: {task.user_id}</p>
     </div>
   </div>;
+}
+
+function UserList(props) {
+  let rows = _.map(props.users, (uu) => <User key={uu.id} user={uu} />);
+  return <div className="row">
+    <div className="col-12">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  </div>;
+}
+
+function User(props) {
+  let {user} = props;
+  return <tr>
+    <td>{user.email}</td>
+  </tr>;
 }

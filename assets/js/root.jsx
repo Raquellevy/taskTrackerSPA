71e018830374723db9aa2ebb11f64be425ef3848
Raquel Levy <levy.r@husky.neu.edu>
2018-11-06 23:42:41 -0500

    add all necessary UI elements

--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -21,20 +21,6 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
     this.fetch_tasks();
   }
 
-
-  fetch_users() {
-    $.ajax("/api/v1/users", {
-      method: "get",
-      dataType: "json",
-      contentType: "application/json; charset=UTF-8",
-      data: "",
-      success: (resp) => {
-        let state1 = _.assign({}, this.state, { users: resp.data });
-        this.setState(state1);
-      },
-    });
-  }
-
   fetch_tasks() {
     $.ajax("/api/v1/tasks", {
       method: "get",
@@ -61,6 +47,18 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
     });
   }
 
+  register(email, pass) {
+    $.ajax("/api/v1/users", {
+      method: "post",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: JSON.stringify({email: email, password_hash: pass}),
+      success: (resp) => {
+        this.create_session(email, pass);
+      }
+    });
+  }
+
   create_task() {
     let newTask = {title:"Title", description:"Description", completed:false, timespent:0, user_id:null}
     $.ajax("/api/v1/tasks", {
@@ -75,30 +73,6 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
     });
   }
 
-  create_user(email, pass) {
-    $.ajax("/api/v1/users", {
-      method: "post",
-      dataType: "json",
-      contentType: "application/json; charset=UTF-8",
-      data: JSON.stringify({email: email, password_hash: pass}),
-      success: (resp) => {
-        let state1 = _.assign({}, this.state, { tasks: resp.data });
-        this.setState(state1);
-      }
-    });
-  }
-
-  fetch_task(id) {
-    let task = {};
-    _map(this.state.tasks, (tt) => {
-      if (tt.id == id ){
-        task = tt;
-      }
-    })
-
-    return task;
-  }
-
   register() {
     let state1 = _.assign({}, this.state, { register: true });
     this.setState(state1);
@@ -106,27 +80,30 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
   render() {
     let login = this.state.register ?
+    // If registration is true, the app is in "registration mode" and we show a registraton form
     <div>
       <h1>Task Tracker</h1>
       <h2>Create Account</h2>
       <div className="form">
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
-        <button className="btn btn-primary" onClick={() => this.create_user("example@blah.blah", "password")}>Register</button>
+        <button className="btn btn-primary" onClick={() => this.register("example@blah.blah", "password")}>Register</button>
       </div>
-    </div> :
+    </div> 
+    :
+    // If registration is false, the app is not in "registration mode" and we show a login form
     <div>
-    <h1>Task Tracker</h1>
-    <div className="form-inline my-2">
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
-      <button className="btn btn-secondary" onClick={() => this.create_session("raquel@example.com", "pass1")}>Login</button>
-    </div>
-    <button className="btn btn-primary" onClick={this.register.bind(this)}>No account? Register</button>
-  </div>
-    ;
+      <h1>Task Tracker</h1>
+      <div className="form-inline my-2">
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button className="btn btn-secondary" onClick={() => this.create_session("raquel@example.com", "pass1")}>Login</button>
+      </div>
+      <button className="btn btn-primary" onClick={this.register.bind(this)}>No account? Register</button>
+    </div>;
 
     let display = this.state.session ? 
+    // if the session is not null, it means a user is currently logged in, so we display the tasks
     <div>
       <Router>
         <div>
@@ -134,13 +111,11 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
           <Route path="/" exact={true} render={() =>
             <TaskList tasks={this.state.tasks} root={this}/>
           } />
-          <Route path="/users" exact={true} render={() =>
-             <UserList users={this.state.users} />
-          } />
         </div>
       </Router>
     </div> 
     : 
+    // if the session is null it means the user is not logged in so we show the login page
     <Router>
       {login}
     </Router>;
@@ -149,10 +124,7 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
   }
 }
 
-/*
-    <div className="col-md-2">
-      <p><Link to={"/users"} onClick={props.root.fetch_users.bind(props.root)}>Users</Link></p>
-    </div>*/
+// Header nav for when a user is logged in, has main link to Task Tracker and log out button
 function Header(props) {
   return <div className="row my-2">
     <div className="col-md-10">
@@ -165,78 +137,78 @@ function Header(props) {
   </div>;
 }
 
+// Component to render a lists of tasks
  function TaskList(props) {
   let tasks = _.map(props.tasks, (t) => <Task key={t.id} task={t} />);
-  return <div>
-    <div className="row my-2">
-      <div className="col-2">
-        <button className="btn btn-primary" onClick={props.root.create_task.bind(props.root)}>New Task</button>
-      </div>
-    </div>
-
-  <div className="row">
-    {tasks}
-  </div>
-  </div>;
+  // Displays form to create a task and the list of tasks below it
+  return (<div>
+            <div className="row">
+              <div className="col-md-2"></div>
+              <div className="col-md-8">
+                <h3>New task</h3>
+                <div className="card">
+                  <div className="card-body">
+                    <form>
+                      <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input type="text" className="form-control" id="title" />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="desc">Description</label>
+                        <textarea className="form-control" id="desc" />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="assignto">Assign to</label>
+                        <select className="form-control" id="assignto">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Create Task</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>____________________________________________________________________________________________________________________________________</p>
+            <h3>Tasks</h3>
+            <div className="row">
+              {tasks}
+            </div>
+         </div>);
 }
+
+// Component to render a single task, with a title, description, timespent, complete flag and user assigned to
  function Task(props) {
   let {task} = props;
-  if (task.edit) {
-    return <div>New Task</div>
-  } else {
-    return <div className="card col-4">
-      <div className="card-body">
-        <h2 className="card-title"><Link to={"/tasks/" + task.id}>{task.title}</Link></h2>
-        <p className="card-text">{task.description}</p>
-        <p>Time logged: {task.timespent}min</p>
-        {task.completed ? <p>Complete!</p> : <p>Incomplete</p>}
-      </div>
-    </div>;
-  }
-}
-
-function TaskForm(props) {
-  return 
-  <div>
-
-  </div>
-  ;
-
-}
-
-function TaskPage(props) {
-  console.log("rendering a task");
-  let task = props.task;
-  return (
-    <div>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
-    </div>
-
-  );
-}
-
-function UserList(props) {
-  let rows = _.map(props.users, (uu) => <User key={uu.id} user={uu} />);
-  return <div className="row">
-    <div className="col-12">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    </div>
-  </div>;
-}
-
-function User(props) {
-  let {user} = props;
-  return <tr>
-    <td>{user.email}</td>
-  </tr>;
+  return <div className="card col-12">
+          <div className="card-body">
+            <h2 className="card-title">{task.title}</h2>
+            <p className="card-text">{task.description}</p>
+            <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Timespent</label>
+                <input type="number" className="form-control" id="exampleInputPassword1" value={task.timespent}/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlSelect1">Assign to</label>
+                <select className="form-control" id="exampleFormControlSelect1">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" checked={task.completed} id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Complete</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+          </div>
+       </div>;
 }

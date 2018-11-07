70a2a5176dd6de05cfb2695c0b8483b51c82f494
Raquel Levy <levy.r@husky.neu.edu>
2018-11-07 02:10:16 -0500

    onchange handlers added for form inputs some styling changes

--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -14,9 +14,9 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
     super(props);
     this.state = {
       tasks: props.tasks,
-      users: [],
       register: false,
       session: null,
+      newTask: {title:"", description:"", completed: false, timespent:0, user_id:0}
     };
     this.fetch_tasks();
   }
@@ -60,9 +60,22 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
   }
 
   create_task() {
-    let newTask = {title:"Title", description:"Description", completed:false, timespent:0, user_id:null}
     $.ajax("/api/v1/tasks", {
       method: "post",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: JSON.stringify(this.state.newTask),
+      success: (resp) => {
+        let state1 = _.assign({}, this.state, { tasks: resp.data });
+        this.setState(state1);
+      }
+    });
+  }
+
+  edit_task(id) {
+    let newTask = this.getTask(id);
+    $.ajax("/api/v1/tasks/" + id, {
+      method: "put",
       dataType: "json",
       contentType: "application/json; charset=UTF-8",
       data: JSON.stringify(newTask),
@@ -78,6 +91,69 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
     this.setState(state1);
   }
 
+  getTask(id) {
+    let tasks = this.state.tasks;
+    let task = null;
+    _.map(tasks, (tt) => {
+      if (tt.id == id) {
+        task = tt;
+      }
+    });
+    return task;
+  }
+
+  updateTask(id, newTask) {
+    let tasks = this.state.tasks;
+    let new_tasks = _.map(tasks, (tt) => {
+      return tt.id == id ? newTask : tt;
+    })
+
+    let state1 = _.assign({}, this.state, { tasks: new_tasks });
+    this.setState(state1);
+  }
+
+  logTime(taskid, time) {
+    let task = this.getTask(taskid);
+    task.timespent = parseInt(time);
+    this.updateTask(taskid, task);
+  }
+
+  togglecomplete(taskid) {
+    let task = this.getTask(taskid);
+    task.completed = !task.completed;
+    this.updateTask(taskid, task);
+  }
+
+  assign(taskid, userid) {
+    let task = this.getTask(taskid);
+    task.user_id = userid;
+    this.updateTask(taskid, task);
+  }
+
+  newtask_title(title) {
+    let task = this.state.newTask;
+    task.title = title;
+    console.log(title);
+    let state1 = _.assign({}, this.state, { newTask: task });
+    this.setState(state1);
+  }
+
+  newtask_description(description) {
+    let task = this.state.newTask;
+    task.description = description;
+    console.log(description);
+    let state1 = _.assign({}, this.state, { newTask: task });
+    this.setState(state1);
+  }
+
+  newtask_assignment(uid) {
+    let task = this.state.newTask;
+    task.user_id = uid;
+    console.log(task)
+    let state1 = _.assign({}, this.state, { newTask: task });
+    this.setState(state1);
+  }
+
   render() {
     let login = this.state.register ?
     // If registration is true, the app is in "registration mode" and we show a registraton form
@@ -87,7 +163,7 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
       <div className="form">
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
-        <button className="btn btn-primary" onClick={() => this.register("example@blah.blah", "password")}>Register</button>
+        <button className="btn btn-info" onClick={() => this.register("example@blah.blah", "password")}>Register</button>
       </div>
     </div> 
     :
@@ -99,7 +175,7 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
         <input type="password" placeholder="password" />
         <button className="btn btn-secondary" onClick={() => this.create_session("raquel@example.com", "pass1")}>Login</button>
       </div>
-      <button className="btn btn-primary" onClick={this.register.bind(this)}>No account? Register</button>
+      <button className="btn btn-info" onClick={this.register.bind(this)}>No account? Register</button>
     </div>;
 
     let display = this.state.session ? 
@@ -109,7 +185,7 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
         <div>
           <Header root={this} />
           <Route path="/" exact={true} render={() =>
-            <TaskList tasks={this.state.tasks} root={this}/>
+            <TaskList newTask={this.state.newTask} tasks={this.state.tasks} root={this}/>
           } />
         </div>
       </Router>
@@ -128,7 +204,7 @@ import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 function Header(props) {
   return <div className="row my-2">
     <div className="col-md-10">
-      <h1><Link to={"/"}>Task Tracker</Link></h1>
+      <h1>Task Tracker</h1>
     </div>
 
     <div className="col-md-2">
@@ -139,27 +215,37 @@ function Header(props) {
 
 // Component to render a lists of tasks
  function TaskList(props) {
-  let tasks = _.map(props.tasks, (t) => <Task key={t.id} task={t} />);
+  let newTask = props.newTask;
+  let newTitle = (ev) => {
+    props.root.newtask_title(ev.target.value);
+  };
+  let newDesc = (ev) => {
+    props.root.newtask_description(ev.target.value);
+  };
+  let newAssign = (ev) => {
+    props.root.newtask_assignment(ev.target.value);
+  };  
+  let tasks = _.map(props.tasks, (t) => <Task key={t.id} task={t} root={props.root}/>);
   // Displays form to create a task and the list of tasks below it
   return (<div>
             <div className="row">
               <div className="col-md-2"></div>
               <div className="col-md-8">
-                <h3>New task</h3>
-                <div className="card">
-                  <div className="card-body">
+                <div className="card bg-light border-info mb-3">
+                  <h2 className="card-header">New Task</h2>
+                  <div className="card-body text-info">
                     <form>
                       <div className="form-group">
                         <label htmlFor="title">Title</label>
-                        <input type="text" className="form-control" id="title" />
+                        <input type="text" className="form-control" id="title" value={newTask.title} onChange={newTitle}/>
                       </div>
                       <div className="form-group">
                         <label htmlFor="desc">Description</label>
-                        <textarea className="form-control" id="desc" />
+                        <textarea className="form-control" id="desc" value={newTask.description} onChange={newDesc}/>
                       </div>
                       <div className="form-group">
                         <label htmlFor="assignto">Assign to</label>
-                        <select className="form-control" id="assignto">
+                        <select className="form-control" id="assignto" value={newTask.user_id} onChange={newAssign}>
                           <option>1</option>
                           <option>2</option>
                           <option>3</option>
@@ -167,14 +253,12 @@ function Header(props) {
                           <option>5</option>
                         </select>
                       </div>
-                      <button type="submit" className="btn btn-primary">Create Task</button>
                     </form>
+                    <button className="btn btn-info" onClick={() => props.root.create_task()}>Create Task</button>
                   </div>
                 </div>
               </div>
             </div>
-            <p>____________________________________________________________________________________________________________________________________</p>
-            <h3>Tasks</h3>
             <div className="row">
               {tasks}
             </div>
@@ -183,19 +267,28 @@ function Header(props) {
 
 // Component to render a single task, with a title, description, timespent, complete flag and user assigned to
  function Task(props) {
-  let {task} = props;
-  return <div className="card col-12">
+  let {root, task} = props;
+  let timechanged = (ev) => {
+    root.logTime(task.id, ev.target.value);
+  };
+  let completechanged = (ev) => {
+    root.togglecomplete(task.id);
+  };
+  let assignchanged = (ev) => {
+    root.assign(task.id, ev.target.value);
+  };  
+  return <div className="card text-white bg-secondary mb-3 col-12">
+          <h2 className="card-header">{task.title}</h2>
           <div className="card-body">
-            <h2 className="card-title">{task.title}</h2>
             <p className="card-text">{task.description}</p>
             <form>
               <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Timespent</label>
-                <input type="number" className="form-control" id="exampleInputPassword1" value={task.timespent}/>
+                <label htmlFor="logtime">Timespent</label>
+                <input type="number" className="form-control" id="logtime" step={15} value={task.timespent} onChange={timechanged}/>
               </div>
               <div className="form-group">
-                <label htmlFor="exampleFormControlSelect1">Assign to</label>
-                <select className="form-control" id="exampleFormControlSelect1">
+                <label htmlFor="assign">Assign to</label>
+                <select className="form-control" id="assign" onChange={assignchanged}>
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -204,11 +297,11 @@ function Header(props) {
                 </select>
               </div>
               <div className="form-check">
-                <input type="checkbox" className="form-check-input" checked={task.completed} id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Complete</label>
+                <input type="checkbox" className="form-check-input" checked={task.completed} id="completed" onChange={completechanged}/>
+                <label className="form-check-label" htmlFor="completed">Complete</label>
               </div>
-              <button type="submit" className="btn btn-primary">Save</button>
             </form>
+            <button className="btn btn-info" onClick={() => root.edit_task(task.id)}>Save</button>
           </div>
        </div>;
 }
